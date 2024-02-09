e25ed60fee49bcf239d767209bf701af66b5f554
Evgeniy Telepnev <telepnef@yandex.ru>
2024-02-09 21:03:15 +0300

    выполнена структуризация кода

---
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -0,0 +1,16 @@
+package ru.stqa.geometry;
+
+
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Square.printSquareArea(3.0);
+        Rectangle.printRectangleArea(5.0, 9.0);
+
+//        double sum = Math.sqrt(5);
+//        System.out.println("sum = " + sum);
+    }
+
+}
