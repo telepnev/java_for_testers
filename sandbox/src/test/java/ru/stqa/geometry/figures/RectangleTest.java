a2db86c4babdfb339f892188a518bf47e53d6f00
Evgeniy Telepnev <telepnef@yandex.ru>
2024-02-10 09:29:16 +0300

    добавлены тест на класс Triangle

--- sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTest.java
@@ -3,8 +3,6 @@ package ru.stqa.geometry.figures;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RectangleTest {
     @Test
     void rectangle() {
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
@@ -3,13 +3,10 @@ package ru.stqa.geometry.figures;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TriangleTest {
 
     @Test
     void canCalculateAre() {
-        double result = Triangle.triangle(6.0, 5.0, 2.2);
-        Assertions.assertEquals(5.28, result);
+        Assertions.assertEquals(5.28, new Triangle(6.0, 5.0, 2.2).triangle());
     }
 }
