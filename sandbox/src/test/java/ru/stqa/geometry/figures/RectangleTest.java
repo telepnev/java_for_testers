d01d74c575e5b155154c49e01787e7582702dc62
Evgeniy Telepnev <telepnef@yandex.ru>
2024-02-10 12:23:15 +0300

    Задание №2

--- sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/RectangleTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class RectangleTest {
     @Test
     void rectangle() {
-        Assertions.assertEquals(20.0, new Rectangle(4.0, 5.0).rectangle());
+        Assertions.assertEquals(20.0, new Rectangle(4.0, 5.0, 2.0).rectangle());
     }
 
 }
